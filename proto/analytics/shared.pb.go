c2489465127c8d076559323b19e212beef60e95d
Easlee Liu <easlee@outlook.com>
2022-01-06 11:09:28 +0000

    remove unused message

--- proto/analytics/shared.pb.go
+++ proto/analytics/shared.pb.go
@@ -228,134 +228,6 @@ func (x *Agent) GetProfile() string {
 	return ""
 }
 
-// 参数
-type Parameter struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Key   string `protobuf:"bytes,1,opt,name=key,proto3" json:"key,omitempty"`     // 键
-	Value string `protobuf:"bytes,2,opt,name=value,proto3" json:"value,omitempty"` // 值
-	Type  string `protobuf:"bytes,3,opt,name=type,proto3" json:"type,omitempty"`   // 类型
-}
-
-func (x *Parameter) Reset() {
-	*x = Parameter{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_proto_analytics_shared_proto_msgTypes[3]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *Parameter) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*Parameter) ProtoMessage() {}
-
-func (x *Parameter) ProtoReflect() protoreflect.Message {
-	mi := &file_proto_analytics_shared_proto_msgTypes[3]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use Parameter.ProtoReflect.Descriptor instead.
-func (*Parameter) Descriptor() ([]byte, []int) {
-	return file_proto_analytics_shared_proto_rawDescGZIP(), []int{3}
-}
-
-func (x *Parameter) GetKey() string {
-	if x != nil {
-		return x.Key
-	}
-	return ""
-}
-
-func (x *Parameter) GetValue() string {
-	if x != nil {
-		return x.Value
-	}
-	return ""
-}
-
-func (x *Parameter) GetType() string {
-	if x != nil {
-		return x.Type
-	}
-	return ""
-}
-
-// 事件
-type Event struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Id        string       `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`               // 事件唯一ID
-	Alias     string       `protobuf:"bytes,2,opt,name=alias,proto3" json:"alias,omitempty"`         // 事件显示名
-	Parameter []*Parameter `protobuf:"bytes,3,rep,name=parameter,proto3" json:"parameter,omitempty"` // 事件参数
-}
-
-func (x *Event) Reset() {
-	*x = Event{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_proto_analytics_shared_proto_msgTypes[4]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *Event) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*Event) ProtoMessage() {}
-
-func (x *Event) ProtoReflect() protoreflect.Message {
-	mi := &file_proto_analytics_shared_proto_msgTypes[4]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use Event.ProtoReflect.Descriptor instead.
-func (*Event) Descriptor() ([]byte, []int) {
-	return file_proto_analytics_shared_proto_rawDescGZIP(), []int{4}
-}
-
-func (x *Event) GetId() string {
-	if x != nil {
-		return x.Id
-	}
-	return ""
-}
-
-func (x *Event) GetAlias() string {
-	if x != nil {
-		return x.Alias
-	}
-	return ""
-}
-
-func (x *Event) GetParameter() []*Parameter {
-	if x != nil {
-		return x.Parameter
-	}
-	return nil
-}
-
 var File_proto_analytics_shared_proto protoreflect.FileDescriptor
 
 var file_proto_analytics_shared_proto_rawDesc = []byte{
@@ -385,20 +257,10 @@ var file_proto_analytics_shared_proto_rawDesc = []byte{
 	0x6f, 0x64, 0x65, 0x6c, 0x12, 0x1e, 0x0a, 0x0a, 0x64, 0x65, 0x76, 0x69, 0x63, 0x65, 0x54, 0x79,
 	0x70, 0x65, 0x18, 0x07, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0a, 0x64, 0x65, 0x76, 0x69, 0x63, 0x65,
 	0x54, 0x79, 0x70, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x70, 0x72, 0x6f, 0x66, 0x69, 0x6c, 0x65, 0x18,
-	0x08, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x70, 0x72, 0x6f, 0x66, 0x69, 0x6c, 0x65, 0x22, 0x47,
-	0x0a, 0x09, 0x50, 0x61, 0x72, 0x61, 0x6d, 0x65, 0x74, 0x65, 0x72, 0x12, 0x10, 0x0a, 0x03, 0x6b,
-	0x65, 0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03, 0x6b, 0x65, 0x79, 0x12, 0x14, 0x0a,
-	0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x76, 0x61,
-	0x6c, 0x75, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x74, 0x79, 0x70, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28,
-	0x09, 0x52, 0x04, 0x74, 0x79, 0x70, 0x65, 0x22, 0x57, 0x0a, 0x05, 0x45, 0x76, 0x65, 0x6e, 0x74,
-	0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64,
-	0x12, 0x14, 0x0a, 0x05, 0x61, 0x6c, 0x69, 0x61, 0x73, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52,
-	0x05, 0x61, 0x6c, 0x69, 0x61, 0x73, 0x12, 0x28, 0x0a, 0x09, 0x70, 0x61, 0x72, 0x61, 0x6d, 0x65,
-	0x74, 0x65, 0x72, 0x18, 0x03, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x0a, 0x2e, 0x50, 0x61, 0x72, 0x61,
-	0x6d, 0x65, 0x74, 0x65, 0x72, 0x52, 0x09, 0x70, 0x61, 0x72, 0x61, 0x6d, 0x65, 0x74, 0x65, 0x72,
-	0x42, 0x1b, 0x5a, 0x19, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x61, 0x6e, 0x61, 0x6c, 0x79, 0x74,
-	0x69, 0x63, 0x73, 0x3b, 0x61, 0x6e, 0x61, 0x6c, 0x79, 0x74, 0x69, 0x63, 0x73, 0x62, 0x06, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x08, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x70, 0x72, 0x6f, 0x66, 0x69, 0x6c, 0x65, 0x42, 0x1b,
+	0x5a, 0x19, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x61, 0x6e, 0x61, 0x6c, 0x79, 0x74, 0x69, 0x63,
+	0x73, 0x3b, 0x61, 0x6e, 0x61, 0x6c, 0x79, 0x74, 0x69, 0x63, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x33,
 }
 
 var (
@@ -413,22 +275,19 @@ func file_proto_analytics_shared_proto_rawDescGZIP() []byte {
 	return file_proto_analytics_shared_proto_rawDescData
 }
 
-var file_proto_analytics_shared_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
+var file_proto_analytics_shared_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_proto_analytics_shared_proto_goTypes = []interface{}{
 	(*Status)(nil),        // 0: Status
 	(*BlankResponse)(nil), // 1: BlankResponse
 	(*Agent)(nil),         // 2: Agent
-	(*Parameter)(nil),     // 3: Parameter
-	(*Event)(nil),         // 4: Event
 }
 var file_proto_analytics_shared_proto_depIdxs = []int32{
 	0, // 0: BlankResponse.status:type_name -> Status
-	3, // 1: Event.parameter:type_name -> Parameter
-	2, // [2:2] is the sub-list for method output_type
-	2, // [2:2] is the sub-list for method input_type
-	2, // [2:2] is the sub-list for extension type_name
-	2, // [2:2] is the sub-list for extension extendee
-	0, // [0:2] is the sub-list for field type_name
+	1, // [1:1] is the sub-list for method output_type
+	1, // [1:1] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_proto_analytics_shared_proto_init() }
@@ -473,30 +332,6 @@ func file_proto_analytics_shared_proto_init() {
 				return nil
 			}
 		}
-		file_proto_analytics_shared_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*Parameter); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_proto_analytics_shared_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*Event); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -504,7 +339,7 @@ func file_proto_analytics_shared_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_proto_analytics_shared_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   5,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
